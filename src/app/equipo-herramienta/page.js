e9539fcbc1c401836ffb5b19dae360fe8b37985b
j-montoya-ei <j.montoya@ei.com.co>
2026-08-04 14:41:31 -0500

    Update page.js

--- src/app/equipo-herramienta/page.js
+++ src/app/equipo-herramienta/page.js
@@ -2,6 +2,8 @@
 
 import { useState, useEffect } from "react";
 import { supabase } from "../../lib/supabase";
+import ImportarDatos from "../../components/ImportarDatos";
+
 
 export default function EquipoPage() {
   const [items, setItems] = useState([]);
