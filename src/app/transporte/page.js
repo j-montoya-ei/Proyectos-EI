7b8c7a82b12cccb8621aa58783a5143174d42e2f
j-montoya-ei <j.montoya@ei.com.co>
2026-08-04 14:42:33 -0500

    Update page.js

--- src/app/transporte/page.js
+++ src/app/transporte/page.js
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { supabase } from "../../lib/supabase";
+import ImportarDatos from "../../components/ImportarDatos";
 
 export default function TransportePage() {
   const [items, setItems] = useState([]);
